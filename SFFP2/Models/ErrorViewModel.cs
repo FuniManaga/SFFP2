d777266a70a46cd79f73b8395b3f0dffb3432db0
WWW <WWW@DESKTOP-149PCLG>
2023-03-11 12:37:56 +0530

    Add project files.

---
+++ SFFP2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SFFP2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
